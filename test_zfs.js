eac8a3971379ee6b699a28fa2cbe810a3fe7f412
orlando <orlando@orlando-solaris-vm.(none)>
2010-04-01 20:08:08 -0700

    improve code and unit tests

--- test_zfs.js
+++ test_zfs.js
@@ -1,13 +1,146 @@
+#!/usr/bin/env node
+
 var sys = require('sys')
-  , zfs = require('./zfs');
+  , assert = require('assert')
+  , zfs = require('./zfs').zfs
+  , zpool = require('./zfs').zpool;
+
+// assert test is running as root
+
+TestPlanner = function (testCount) {
+  this.count = 0;
+  var self = this;
+  var onExit = function (error) {
+    if (error) { 
+      puts(error.stack);
+    }
+    if (self.teardown) {
+      puts("Tearing down...");
+      self.teardown();
+    }
+    assert.equal(testCount, self.count,
+                 'Number of tests run (' + self.count
+                 + ') didn\'t match number of tests planned (' + testCount + ')');
+    puts("All tests successful!");
+  };
+
+  process.addListener('exit', onExit);
+  process.addListener('uncaughtException', onExit);
+};
+
+TestPlanner.prototype.track = function (fn) {
+  var self = this;
+  return function () {
+    puts("test-");
+    self.count++;
+    return fn.apply(undefined, arguments);
+  };
+};
+
+var testPlan = 15;
+var tp = new TestPlanner(testPlan);
+
+tp.teardown = function () {
+  puts("tearing down");
+  zfs.destroyAll(zfsName, function () {
+    puts("destroyed " + zfsName + inspect(arguments));
+  });
+}
+
+ok = tp.track(assert.ok);
+equal = tp.track(assert.equal);
 
 var puts = sys.puts;
 var inspect = sys.inspect;
 
-zfs.zpool.list(function (out) {
-  puts(inspect(out));
-});
+var zfsName = process.argv[2] || 'node-zfs-test/test';
+var zpoolName = zfsName.split('/')[0];
 
-zfs.zfs.list(function (out) {
-  puts(inspect(out));
-});
+function preCheck() {
+  // check zpool exists
+  zpool.list(function (err, fields, list) {
+    ok(list);
+    ok(list.length > 0, "zpool list is empty");
+    ok(list.some(function (i) { return i[0] == zpoolName; }),
+       "zpool doesn't exist");
+
+    zfs.list(function (err, fields, list) {
+      ok(list.length > 0, "zfs list is empty");
+      ok(!list.some(function (i) { return i[0] == zfsName; }),
+         "zfs dataset already exists");
+
+      runTests();
+    });
+  });
+}
+
+function assertDatasetExists(name, callback) {
+  zfs.list(name, function (err, fields, list) {
+    ok(list.length > 0, "zfs list is empty");
+    ok(list.some(function (i) { return i[0] == name; }),
+       "zfs dataset doesn't exist");
+    callback();
+  });
+}
+
+function assertDatasetDoesNotExist(name, callback) {
+  zfs.list(name, function (err, fields, list) {
+    ok(err);
+    ok(err.msg.match(/does not exist/));
+    ok(!list, "zfs list is empty");
+    callback();
+  });
+}
+
+assertDatasetExists = tp.track(assertDatasetExists);
+assertDatasetDoesNotExist = tp.track(assertDatasetDoesNotExist);
+
+function runTests() {
+  var tp = 0;
+
+  var tests =
+    [ function () {
+        zfs.create(zfsName, function () {
+          assertDatasetExists(zfsName, next);
+        });
+      }
+    , function () {
+      var snapshotName = zfsName + '@mysnapshot';
+      zfs.snapshot(snapshotName, function (err, stdout, stderr) {
+        assertDatasetExists(snapshotName, next);
+      });
+    }
+    , function () {
+      var snapshotName = zfsName + '@mysnapshot';
+      zfs.destroy(snapshotName, function (err, stdout, stderr) {
+        assertDatasetDoesNotExist(snapshotName, next);
+      });
+    }
+
+    
+//     , function () {}
+//     , function () {}
+    ];
+
+  function next() {
+    if (tp >= tests.length) return;
+
+    puts("Starting new test");
+    try {
+      tests[tp++]();
+    }
+
+    catch(e) {
+      puts("Error: " + e.msg);
+      next();
+    }
+  }
+
+  next();
+}
+
+function test() {
+  preCheck();
+}
+
+test();
--- zfs.js
+++ zfs.js
@@ -1,5 +1,5 @@
 var sys   = require('sys')
-  , spawn = require('child_process').spawn;
+  , exec  = require('child_process').exec;
 
 var puts = sys.puts;
 
@@ -11,18 +11,29 @@ exports.zpool = zpool = function () { }
 zpool.listFields_ = [ 'name', 'size', 'used', 'available'
                     , 'capacity', 'health', 'altroot' ];
 
-zpool.list = function (callback) {
-  var proc = spawn(ZPOOL_PATH, ['list', '-H']);
-  var buffer = '';
+zpool.list = function () {
+  var dataset='', callback;
+  switch (arguments.length) {
+    case 1:
+      callback = arguments[0];
+      break;
+    case 2:
+      dataset = arguments[0];
+      callback = arguments[1];
+      break;
+    default:
+      throw Error("Invalid arguments");
+  }
 
-  proc.stdout.addListener('data', function (data) {
-    buffer = buffer + data;
-  });
-
-  proc.addListener('exit', function (code) {
-    buffer = buffer.replace(/(^\s+|\s+$)/, '');
-    lines = parseTabSeperatedTable(buffer);
-    callback({ fields: zpool.listFields_, zpools: lines });
+  exec(ZPOOL_PATH + " list -H " + dataset, function (err, stdout, stderr) {
+    stdout = stdout.trim();
+    if (err) {
+      err.msg = stderr;
+      callback(err);
+      return;
+    }
+    lines = parseTabSeperatedTable(stdout);
+    callback(err, zpool.listFields_, lines);
   });
 };
 
@@ -39,19 +50,63 @@ function parseTabSeperatedTable(data) {
 
 exports.zfs = zfs = function () {}
 
+zfs.create = function (name, callback) {
+  if (arguments.length != 2) {
+    throw Error("Invalid arguments");
+  }
+
+  exec(ZFS_PATH + " create " + name, callback);
+}
+
+zfs.snapshot = function (name, callback) {
+  if (arguments.length != 2) {
+    throw Error("Invalid arguments");
+  }
+
+  exec(ZFS_PATH + " snapshot " + name, callback);
+}
+
+zfs.destroy = function (name, callback) {
+  if (arguments.length != 2) {
+    throw Error("Invalid arguments");
+  }
+
+  exec(ZFS_PATH + " destroy " + name, callback);
+}
+
+zfs.destroyAll = function (name, callback) {
+  if (arguments.length != 2) {
+    throw Error("Invalid arguments");
+  }
+
+  exec(ZFS_PATH + " destroy -r " + name, callback);
+}
+
 zfs.listFields_ = [ 'name', 'used', 'available', 'referenced', 'mountpoint' ];
 
-zfs.list = function (callback) {
-  var proc = spawn(ZFS_PATH, ['list', '-H']);
-  var buffer = '';
+// zfs.list(callback) - list all datasets
+// zfs.list(dataset, callback) -  list specific dataset
+zfs.list = function () {
+  var dataset='', callback;
+  switch (arguments.length) {
+    case 1:
+      callback = arguments[0];
+      break;
+    case 2:
+      dataset = arguments[0];
+      callback = arguments[1];
+      break;
+    default:
+      throw Error("Invalid arguments");
+  }
 
-  proc.stdout.addListener('data', function (data) {
-    buffer = buffer + data;
-  });
-
-  proc.addListener('exit', function (code) {
-    buffer = buffer.replace(/(^\s+|\s+$)/, '');
-    lines = parseTabSeperatedTable(buffer);
-    callback({ fields: zfs.listFields_, zfs: lines });
+  exec(ZFS_PATH + " list -t all -H " + dataset, function (err, stdout, stderr) {
+    stdout = stdout.trim();
+    if (err) {
+      err.msg = stderr;
+      return callback(err);
+    }
+    lines = parseTabSeperatedTable(stdout);
+    callback(err, zfs.listFields_, lines);
   });
 };
