b819c24ed97dea8a10689b8191c740866141ce34
Orlando Vazquez <ovazquez@gmail.com>
2010-04-14 06:25:24 -0700

    Use execFile instead of exec - use an array of command line parameters instead of a string - use timeouts to prevent commands from running for too long

--- zfs.js
+++ zfs.js
@@ -1,5 +1,5 @@
-var sys   = require('sys')
-  , exec  = require('child_process').exec;
+var sys      = require('sys')
+  , execFile = require('child_process').execFile;
 
 var puts = sys.puts;
 
@@ -8,8 +8,11 @@ var ZPOOL_PATH = '/usr/sbin/zpool'
 
 exports.zpool = zpool = function () { }
 
-zpool.listFields_ = [ 'name', 'size', 'used', 'available'
-                    , 'capacity', 'health', 'altroot' ];
+zpool.listFields_ =
+    [ 'name', 'size', 'used', 'available' , 'capacity', 'health', 'altroot' ];
+
+// if zfs commands take longer than timeoutDuration it's an error
+timeoutDuration = exports.timeoutDuration = 5000;
 
 zpool.list = function () {
   var dataset='', callback;
@@ -22,10 +25,11 @@ zpool.list = function () {
       callback = arguments[1];
       break;
     default:
-      throw Error("Invalid arguments");
+      throw Error('Invalid arguments');
   }
 
-  exec(ZPOOL_PATH + " list -H " + dataset, function (err, stdout, stderr) {
+  execFile(ZPOOL_PATH, ['list', '-H', 'dataset'], { timeout: timeoutDuration },
+    function (err, stdout, stderr) {
     stdout = stdout.trim();
     if (err) {
       err.msg = stderr;
@@ -49,30 +53,30 @@ exports.zfs = zfs = function () {}
 
 zfs.create = function (name, callback) {
   if (arguments.length != 2) {
-    throw Error("Invalid arguments");
+    throw Error('Invalid arguments');
   }
-  exec(ZFS_PATH + " create " + name, callback);
+  execFile(ZFS_PATH, ['create', name], { timeout: timeoutDuration }, callback);
 }
 
 zfs.snapshot = function (name, callback) {
   if (arguments.length != 2) {
-    throw Error("Invalid arguments");
+    throw Error('Invalid arguments');
   }
-  exec(ZFS_PATH + " snapshot " + name, callback);
+  execFile(ZFS_PATH, ['snapshot', name], { timeout: timeoutDuration }, callback);
 }
 
 zfs.destroy = function (name, callback) {
   if (arguments.length != 2) {
-    throw Error("Invalid arguments");
+    throw Error('Invalid arguments');
   }
-  exec(ZFS_PATH + " destroy " + name, callback);
+  execFile(ZFS_PATH, ['destroy', name], { timeout: timeoutDuration }, callback);
 }
 
 zfs.destroyAll = function (name, callback) {
   if (arguments.length != 2) {
-    throw Error("Invalid arguments");
+    throw Error('Invalid arguments');
   }
-  exec(ZFS_PATH + " destroy -r " + name, callback);
+  execFile(ZFS_PATH, ['destroy', '-r',  name], { timeout: timeoutDuration }, callback);
 }
 
 zfs.listFields_ = [ 'name', 'used', 'available', 'referenced', 'mountpoint' ];
@@ -80,7 +84,7 @@ zfs.listFields_ = [ 'name', 'used', 'available', 'referenced', 'mountpoint' ];
 // zfs.list(callback) - list all datasets
 // zfs.list(dataset, callback) -  list specific dataset
 zfs.list = function () {
-  var dataset='', callback;
+  var dataset='', options={}, callback;
   switch (arguments.length) {
     case 1:
       callback = arguments[0];
@@ -90,16 +94,18 @@ zfs.list = function () {
       callback = arguments[1];
       break;
     default:
-      throw Error("Invalid arguments");
+      throw Error('Invalid arguments');
   }
 
-  exec(ZFS_PATH + " list -t all -H " + dataset, function (err, stdout, stderr) {
-    stdout = stdout.trim();
-    if (err) {
-      err.msg = stderr;
-      return callback(err);
-    }
-    lines = parseTabSeperatedTable(stdout);
-    callback(err, zfs.listFields_, lines);
-  });
+  execFile(ZFS_PATH, ['list', '-t', 'all', '-H', dataset],
+    { timeout: timeoutDuration },
+    function (err, stdout, stderr) {
+      stdout = stdout.trim();
+      if (err) {
+        err.msg = stderr;
+        return callback(err);
+      }
+      lines = parseTabSeperatedTable(stdout);
+      callback(err, zfs.listFields_, lines);
+    });
 };
